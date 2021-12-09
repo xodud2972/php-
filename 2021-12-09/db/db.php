ad907d653f0325f5af6de6a387cda62a3725a3eb
xodud2972 <75707477+xodud2972@users.noreply.github.com>
2021-12-09 23:21:46 +0900

    20212-12-09 Array처리 미해결
    20212-12-09

---
+++ 2021-12-09/db/db.php
@@ -0,0 +1,3 @@
+<?php
+    $conn = mysqli_connect('localhost:3307', 'root', '111111', 'peopledb') or die (mysqli_connect_error($conn));
+?>
---
+++ 2021-12-09/process/process_insert.php
@@ -0,0 +1,44 @@
+
+<?php
+include_once('../db/db.php');    
+
+$fname = $_POST['firstname'];
+$lname = $_POST['lastname'];
+$mname = $_POST['Middlename'];
+$ads = $_POST['Address'];
+$ctt = $_POST['Contact'];
+$cmt = $_POST['comment'];
+
+
+$filename = $_FILES['files']['name'];
+$count = count($filename);
+$filepath = "../uploads";
+
+// 업로드를 위한 For문 정상실행
+for($i=0; $i<$count; $i++){
+    $tmp_name = $_FILES["files"]["tmp_name"][$i];
+    $name = basename($_FILES["files"]["name"][$i]);
+    move_uploaded_file($tmp_name, "$filepath/$name");
+}
+
+// // query에 넣기 위한 for문 미완성 아래 출력되는 것 처럼 게시판에 등록이 되어야 함
+// function filename($filename, $count){
+//     for($i=0; $i<$count; $i++){
+//         $filename[$i];
+//     } 
+// }
+
+// print_r(filename($filename, $count));
+
+
+$query = "INSERT INTO people
+            (first_name, last_name, mid_name, address, contact, comment, file)
+            VALUES ('" . $fname . "','" . $lname . "','" . $mname . "','" . $ads . "','" . $ctt . "','" . $cmt . "','" . json_encode($filename) . "')";
+
+mysqli_query($conn, $query) or die('에러입니다');
+?>
+
+<script type="text/javascript">
+    //alert("새로운 회원이 등록되었습니다.");
+    //window.location = "../view/index.php";
+</script>
