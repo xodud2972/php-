8c2aad8464e8a1947ec0a9d8c53380e316022583
xodud2972 <75707477+xodud2972@users.noreply.github.com>
2021-12-28 11:44:50 +0900

    2021-12-28
    2021-12-28

--- 2021-12-28/process/process_del_filelist.php
+++ 2021-12-28/process/process_del_filelist.php
@@ -1,6 +1,5 @@
 
 <?php
-
     include_once('../db/db.php');
     $db = db_open();
 
@@ -9,8 +8,10 @@
         $id = $_GET['file_id'];
         $queryDelFile = sprintf('DELETE FROM t_file WHERE file_id = %s',  $id);
         que($db,$queryDelFile);
-        echo '파일이 성공적으로 삭제되었습니다.';
-        echo '<input type="button" value="뒤로돌아가기" onclick=bakcPage();></input>';
+        echo '<h1>파일이 성공적으로 삭제되었습니다.</h1>';
+        echo '<h2>삭제된 파일의 번호는 '.$id.'입니다. </h2>';
+        echo '<h2>실행된 쿼리는 '.$queryDelFile.'</h2>';
+        echo '<input type="button" value="뒤로돌아가기" onclick=bakcPage(); ></input>';
     }
 ?>
 
